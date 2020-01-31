48aba5ad0e751eb90bc01ed37d6a6b4be68e9f5e
Wendel <wendelsagrilo@gmail.com>
2020-01-31 00:44:08 -0200

    Rotas-Arquitetura
    Ajuste no packge.lock;
    Criação das rotas com redirect para a Home e 404;
    Criação do component que auxiliará na tarefa de listar personagens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Home/home.component';
+import { NotFoundComponent } from './Not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
