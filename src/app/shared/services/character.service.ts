bed2529aaed6793f41e919c914707361ccca3761
Wendel <wendelsagrilo@gmail.com>
2020-02-02 21:55:43 -0200

    Deploy
    Requisição https para o service de characters

--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Character } from '../models/character';
 
-const URL_REQUEST = "http://5dd5b6d7ce4c3000144031aa.mockapi.io/star";
+const URL_REQUEST = "https://5dd5b6d7ce4c3000144031aa.mockapi.io/star";
 
 @Injectable({
   providedIn: 'root'
